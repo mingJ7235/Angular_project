e3a7a0d6ccac259e9c2bd6def2c80e939147cc59
김민재 <joshuara7235@gmail.com>
2021-09-26 15:51:08 +0900

    Practice component made for Angular8

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
+import { PracticeComponent } from './practice/practice.component';
 
 @NgModule({ //데코레이터. 즉 장식자. 
   declarations: [ //컴포넌트
     AppComponent,
-    HelloComponent
+    HelloComponent,
+    PracticeComponent
   ],
   imports: [ //다른 모듈들이 들어옴
     BrowserModule,
---
+++ front-end/src/app/practice/practice.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component ({
+    selector : 'practice',
+    templateUrl : './practice.component.html',
+    styleUrls : ['./practice.component.scss']
+    
+})
+export class PracticeComponent{
+
+
+}
