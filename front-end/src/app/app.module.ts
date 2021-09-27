95a3391b1ed71c2d752ab1a8d5f358beea42490e
김민재 <joshuara7235@gmail.com>
2021-09-27 22:09:37 +0900

    Refactor for module and setting for import modules

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,11 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SectionComponent } from './section/section.component';
-import { TimeDisplayComponent } from './section/stopwatch/time-display/time-display.component';
-import { ButtonsComponent } from './section/stopwatch/buttons/buttons.component';
+
 import { LayoutModule } from './layout/layout.module';
 import { SectionModule } from './section/section.module';
 
--- front-end/src/app/layout/layout.module.ts
+++ front-end/src/app/layout/layout.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TestComponent } from './test/test.component';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+
+
 
 
 
